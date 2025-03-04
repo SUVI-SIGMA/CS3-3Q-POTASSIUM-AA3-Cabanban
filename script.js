1f55711b869accc43336d0c0b6890b8536107067
SUVI-SIGMA <smcabanban22@src.pshs.edu.ph>
2025-03-04 14:50:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function checkMessage() {
     let message = prompt("Enter a message: ");
 
+    if (message === null || message.trim() === "") {
+        document.getElementById('result').innerHTML = `No message entered. Please try again!`;
+        return;
+    }
+
     let hasFR = message.includes("FR") || message.includes("fr");
     let hasAI = message.includes("AI") || message.includes("ai");
     let hasMixedAI = message.includes("aI") || message.includes("Ai");
@@ -9,14 +14,11 @@ function checkMessage() {
         document.getElementById('result').innerHTML = `The message, "${message}" is legitimate!`;
     } 
     else if (hasMixedAI || hasAI) {
-        document.getElementById('result').innerHTML = `The message, "${message}" is fake!`;
+        document.getElementById('result').innerHTML = `The message, "${message}" is tampered with!`;
     } 
     else if (hasFR) {
         document.getElementById('result').innerHTML = `The message, "${message}" is legitimate!`;
-    } 
-    else if (hasAI) {
-        document.getElementById('result').innerHTML = `The message, "${message}" is tampered with!`;
-    } 
+    }
     else {
         document.getElementById('result').innerHTML = `The message, "${message}" is yet to be encoded with!`;
     }
